be9430c5050abe2fa23577af239ba360fec5f84f
didonelucas <144860505+didonelucas@users.noreply.github.com>
2024-10-04 19:22:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-let beat = new Audio('audios/Pedido.mp3');
+let beatPedido = new Audio('audios/Pedido.mp3');
 let beatBurguer = new Audio('audios/x-burguer.mp3');
 let beatBacon = new Audio('audios/x-bacon.mp3');
 let beatFrango = new Audio('audios/x-frango.mp3');
 let beatTudo = new Audio('audios/x-tudo.mp3');
 let beatConfirmacao = new Audio('audios/confirmacao.mp3');
-var fase = 0;
+var fase = 1;
 
-document.addEventListener('keydown', function(event) {
+    document.addEventListener('keydown', function(event) {
     const keyName = event.key;
 
     if (keyName === 'j') {
-        beat.play();
-        fase = 1;
+        if (fase === 1) {
+            beatPedido.play(); 
+        } else if (fase === 2) {
+            beatBurguer.play(); 
+        } else if (fase === 3) {
+            beatBacon.play(); 
+        } else if (fase === 4) {
+            beatFrango.play(); 
+        } else if (fase === 5) {
+            beatTudo.play(); 
+        }
     } else if (keyName === 'g') {
         if (fase === 1) {
-            beatBurguer.play();
+            beatBurguer.play(); 
             fase = 2;
         } else if (fase === 2) {
-            beatBacon.play();
+            beatBacon.play(); 
             fase = 3;
         } else if (fase === 3) {
-            beatFrango.play();
+            beatFrango.play(); 
             fase = 4;
         } else if (fase === 4) {
             beatTudo.play();
@@ -28,11 +37,12 @@ document.addEventListener('keydown', function(event) {
         }
     } else if (keyName === 'a') {
         if (fase === 2 || fase === 3 || fase === 4 || fase === 5) {
-            beatConfirmacao.play();
+            beatConfirmacao.play(); 
             fase++;
         }
     }
 });
+
 
 
 
